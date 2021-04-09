0fe6535b7312f350cfcd8bbc8af4388f8a239eba
Josh Hull <josh.hull.w@gmail.com>
2021-04-09 20:09:12 +1000

    Fix build

---
+++ clients/rxjs-dynamo-client-v3/lib/client.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const client_dynamodb_1 = require("@aws-sdk/client-dynamodb");
+class RxjsDynamoDbClient extends client_dynamodb_1.DynamoDBClient {
+}
+//# sourceMappingURL=client.js.map
