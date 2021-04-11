2f20bca6c067c60e04aa9fbede061cde3399f9e3
Joshua Hull <josh.hull.w@gmail.com>
2021-04-12 06:22:24 +1000

    Add jest

---
+++ test/clients/observable-dynamo-client-v3/lib/ObservableDynamoDB.test.js
@@ -0,0 +1,3 @@
+"use strict";
+test('', () => { });
+//# sourceMappingURL=ObservableDynamoDB.test.js.map
---
+++ test/clients/observable-dynamo-client-v3/src/ObservableDynamoDB.test.ts
@@ -0,0 +1,9 @@
+import { ObservableDynamoDB } from 'observable-dynamo-client-v3';
+
+describe('PutItemCommand tests', () => {
+  test('Should successfully put item', () => {});
+});
+
+describe('GetItemCommand tests', () => {
+  test('Should successfully get item', () => {});
+});
