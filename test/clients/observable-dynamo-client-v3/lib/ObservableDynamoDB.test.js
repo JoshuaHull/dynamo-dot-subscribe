33589f894e630c9d83d484256ea895cd41f75faa
Josh Hull <josh.hull.w@gmail.com>
2021-04-12 07:26:15 +1000

    jest working

--- test/clients/observable-dynamo-client-v3/lib/ObservableDynamoDB.test.js
+++ test/clients/observable-dynamo-client-v3/lib/ObservableDynamoDB.test.js
@@ -1,3 +1,9 @@
 "use strict";
-test('', () => { });
+Object.defineProperty(exports, "__esModule", { value: true });
+describe('PutItemCommand tests', () => {
+    test('Should successfully put item', () => { });
+});
+describe('GetItemCommand tests', () => {
+    test('Should successfully get item', () => { });
+});
 //# sourceMappingURL=ObservableDynamoDB.test.js.map
