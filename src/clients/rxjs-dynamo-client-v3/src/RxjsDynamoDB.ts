a23e5f39045f7cded4d22aaa662587a602c1fec4
Josh Hull <josh.hull.w@gmail.com>
2021-04-12 18:10:39 +1000

    Add licenses

--- src/clients/rxjs-dynamo-client-v3/src/RxjsDynamoDB.ts
+++ src/clients/rxjs-dynamo-client-v3/src/RxjsDynamoDB.ts
@@ -1,3 +1,24 @@
+/*
+  GPL 3.0 License
+
+  RxjsDynamoDB - An Observable DynamoDB Client
+
+  Copyright (C) 2020 Joshua Hull
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 import { ObservableDynamoDBClient } from 'observable-dynamo-client-v3';
 import { DynamoDBClientConfig } from '@aws-sdk/client-dynamodb';
 import { BehaviorSubject } from "rxjs";
